5018381d9b758892b6f0d2729993e493a53a6e6a
ashankk2 <ashankk2@illinois.edu>
2022-05-08 14:10:36 -0500

    ye

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import python_weather
 # local weather information to the user based on a zip code
 # breakdown of activity types
 
+zip_code = None
+mile_goal = None
+
 app = Flask(__name__)
 
 # 61154779 ashank athlete id
@@ -15,17 +18,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 async def home():
-    # zip_code_given = False
-    # with open('api.json', 'a') as f:
-    #     f.write(json.dumps(r))
-    # declare the client. format defaults to metric system (celcius, km/h, etc.)
+    global zip_code
+    global mile_goal
 
     if request.method == 'POST':
-        zip = request.form['zip']
+        zip = str(request.form['zip'])
+        zip_code = zip
         client = python_weather.Client(format=python_weather.IMPERIAL)
 
         # fetch a weather forecast from a city
-        weather = await client.find(str(zip))
+        weather = await client.find(zip)
 
         forecasts = []
         for f in weather.forecasts[2:]:
@@ -38,9 +40,69 @@ async def home():
 
         await client.close()
 
-        return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts)
+        if mile_goal is not None:
+            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, need_miles=False, miles=mile_goal)
+        else:
+            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, need_miles=True)
     else:
-        return render_template('general.html', need_input=True)
+        zip_code = None
+        if mile_goal is not None:
+            return render_template('general.html', need_input=True, need_miles=False, miles=mile_goal)
+        else:
+            return render_template('general.html', need_input=True, need_miles=True)
+
+
+@app.route('/goal', methods=['GET', 'POST'])
+async def goal():
+    global zip_code
+    global mile_goal
+
+    if request.method == 'POST':
+        miles = str(request.form['miles'])
+        mile_goal = miles
+
+        if zip_code is not None:
+            client = python_weather.Client(format=python_weather.IMPERIAL)
+
+            # fetch a weather forecast from a city
+            weather = await client.find(zip_code)
+
+            forecasts = []
+            for f in weather.forecasts[2:]:
+                dic = {}
+                dic['date'] = f.date.strftime("%B %d, %Y")
+                dic['sky_text'] = f.sky_text
+                dic['temperature'] = f.temperature
+
+                forecasts.append(dic)
+
+            await client.close()
+
+            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, miles=miles, need_miles=False)
+        else:
+            return render_template('general.html', need_input=True, miles=miles, need_miles=False)
+    else:
+        mile_goal = None
+        if zip_code is not None:
+            client = python_weather.Client(format=python_weather.IMPERIAL)
+
+            # fetch a weather forecast from a city
+            weather = await client.find(zip_code)
+
+            forecasts = []
+            for f in weather.forecasts[2:]:
+                dic = {}
+                dic['date'] = f.date.strftime("%B %d, %Y")
+                dic['sky_text'] = f.sky_text
+                dic['temperature'] = f.temperature
+
+                forecasts.append(dic)
+
+            await client.close()
+
+            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, need_miles=True)
+        else:
+            return render_template('general.html', need_input=True, need_miles=True)
 
 
 if __name__ == '__main__':
