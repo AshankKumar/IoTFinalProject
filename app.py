10769f06b06b3718145806c179fb229acf620cfd
ameyagh <ameyapg2@illinois.edu>
2022-05-08 16:19:56 -0500

    ye

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request, url_for, redirect
 import requests
 import json
 import python_weather
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+from datetime import datetime, timedelta
+
+matplotlib.use('Agg')
+plt.gcf().subplots_adjust(bottom=0.2)
 
 # average pace, weekly miles ran, sprint times
 # most frequented running routes
@@ -10,6 +17,7 @@ import python_weather
 
 zip_code = None
 mile_goal = None
+total_distance = 0
 
 app = Flask(__name__)
 
@@ -20,6 +28,7 @@ app = Flask(__name__)
 async def home():
     global zip_code
     global mile_goal
+    global total_distance
 
     if request.method == 'POST':
         zip = str(request.form['zip'])
@@ -41,13 +50,13 @@ async def home():
         await client.close()
 
         if mile_goal is not None:
-            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, need_miles=False, miles=mile_goal)
+            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, need_miles=False, miles=mile_goal,  met_goal=total_distance >= float(mile_goal), total_distance=total_distance)
         else:
             return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, need_miles=True)
     else:
         zip_code = None
         if mile_goal is not None:
-            return render_template('general.html', need_input=True, need_miles=False, miles=mile_goal)
+            return render_template('general.html', need_input=True, need_miles=False, miles=mile_goal,  met_goal=total_distance >= float(mile_goal), total_distance=total_distance)
         else:
             return render_template('general.html', need_input=True, need_miles=True)
 
@@ -78,9 +87,9 @@ async def goal():
 
             await client.close()
 
-            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, miles=miles, need_miles=False)
+            return render_template('general.html', need_input=False, current_temp=weather.current.temperature, forecasts=forecasts, miles=miles, need_miles=False, met_goal=total_distance >= float(miles), total_distance=total_distance)
         else:
-            return render_template('general.html', need_input=True, miles=miles, need_miles=False)
+            return render_template('general.html', need_input=True, miles=miles, need_miles=False,  met_goal=total_distance >= float(miles), total_distance=total_distance)
     else:
         mile_goal = None
         if zip_code is not None:
@@ -104,6 +113,48 @@ async def goal():
         else:
             return render_template('general.html', need_input=True, need_miles=True)
 
+def get_df():
+    col_names = ['id','type']
+    activities = pd.DataFrame(columns=col_names)
+    page = 1
+
+    after = get_seven_days_date()
+    global total_distance
+    total_distance = 0
+    while True:
+        url = 'https://www.strava.com/api/v3/athlete/activities'
+        headers = {'Authorization': 'Bearer be3a2cd478c838388bc13ea45cfa92e1e16b4f52'}
+        params = {'page': page, 'after': after}
+
+        r = requests.get(url, headers=headers, params=params)
+        r = r.json()
+
+        if (not r):
+            break
+
+        for x in range(len(r)):
+            activities.loc[x + (page-1)*30,'id'] = r[x]['id']
+            activities.loc[x + (page-1)*30,'type'] = r[x]['type']
+            total_distance += r[x]['distance']
+        page += 1
+    total_distance /= 1609
+    return activities
+
+def activity_graph():
+    df = get_df()
+    x = df['type'].value_counts().plot(kind='bar', color=['blue', 'red', 'orange', 'green', 'purple', 'yellow'])
+
+    plt.savefig('static/plot.png')
+
+
+def get_seven_days_date():
+    date = datetime.today() - timedelta(days=7)
+
+    return date.timestamp()
+
+
 
 if __name__ == '__main__':
+    print(get_seven_days_date())
+    activity_graph()
     app.run(debug=True, port='8888')
